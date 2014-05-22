b0019a983fa45bef39b212507b8a48d0891122a2
lu.shunshun <lu.shunshun@gmail.com>
2014-05-23 00:12:49 +0100

    Initial commit

---
+++ src/test/java/com/inkglobal/exercise/BerlinClockTest.java
@@ -0,0 +1,59 @@
+package com.inkglobal.exercise;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BerlinClockTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void EvenSecond_LampOfSeconds_YellowLampOn () {
+
+    }
+
+    @Test
+    public void OddSecond_LampOfSeconds_YellowLampOff () {
+
+    }
+
+    @Test
+    public void Before5AM_LampsOfHours_NoLampsOn () {
+
+    }
+
+    @Test
+    public void Between5PMAnd20PM_LampsOfHours_OneMoreRedLampOnEvery5Hours () {
+
+    }
+
+    @Test
+    public void After20PM_LampsOfHours_OneMoreRedLampOnEvery1Hour() {
+
+    }
+
+    @Test
+    public void Before15MinutesPast_LampsOfMinutes_NoLampsOn () {
+
+    }
+
+    @Test
+    public void Every15Minutes_LampsOfMinutes_OneMoreRedLampOn () {
+
+    }
+
+    @Test
+    public void Until55Minutes_LampsOfMinutes_OneMoreYellowLampOnEvery5Minutes () {
+
+    }
+
+    @Test
+    public void After55Minutes_LampsOfMinutes_OneMoreYellowLampOnEvery1Minute () {
+
+    }
+}
